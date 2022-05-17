e36f0e0e6dd54af36025104407557d10e8399b53
Paul Anderson <panderson@tenable.com>
2022-05-17 11:10:44 -0600

    Exclude test environment

--- scripts/scannerImportImage.sh
+++ scripts/scannerImportImage.sh
@@ -11,7 +11,17 @@
 IMPORT_REPO_NAME="docker.io/hpcprofessional/accurics"
 IMAGE_NAME="hpcprofessional:1.0.18beta1"
 
+#You'll need to authenticate to the Tenable Container Security Docker Image Repository using special credentails
+#How to get the credentials are described here: https://docs.tenable.com/tenableio/Content/ContainerSecurity/DownloadCSScanner.htm
+#Note that these are not:
+# Your Tenable.io login
+# The API Access/Secret Key
+# (So don't use any of the above)
+echo "Please provde Container Security Registry Authentication Credentials:"
+docker login tenableio-docker-consec-local.jfrog.io
+
 #Pulling the repo first can avoid certain edge cases including high network latency
+#(Also confirms our registry credentials work)
 docker pull $IMPORT_REPO_NAME
 
 docker save $IMPORT_REPO_NAME | docker run \
